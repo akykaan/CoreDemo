82575098369856c81960d87ccca71bd0f2e28c23
akykaan <kaanaky77@gmail.com>
2021-09-21 12:59:44 +0300

    20.ders

---
+++ DataAcessLayer/EntityFramework/EfCommentRepository.cs
@@ -0,0 +1,14 @@
+﻿using DataAcessLayer.Abstract;
+using DataAcessLayer.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace DataAcessLayer.EntityFramework
+{
+	public class EfCommentRepository : GenericRepository<Comment>, ICommentDal
+	{
+	}
+}
